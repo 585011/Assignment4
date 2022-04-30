bacbb79f403447f424124b29c2dd7085b4cbd334
h585002 <alex.oeen@gmail.com>
2022-04-30 19:46:44 +0200

    login

--- BookRoom/BookRoom/Controllers/HomeController.cs
+++ BookRoom/BookRoom/Controllers/HomeController.cs
@@ -1,24 +1,53 @@
-﻿using System;
+﻿using BookRoom.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookRoom.Models;
 
 namespace BookRoom.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(Customers login)
+        {
+            if (ModelState.IsValid)
+            {
+                oblig4Entities9 db = new oblig4Entities9();
+                var user = (from userlist in db.Customers
+                            where userlist.CustomersID == login.CustomersID && userlist.pass == login.pass
+                            select new
+                            {
+                                userlist.CustomersID
+                            }).ToList();
+                if (user.FirstOrDefault() != null)
+                {
+                    Session["CustomersID"] = user.FirstOrDefault().CustomersID;
+                    return Redirect("/bookings/index");
+                }
+
+                else
+                {
+                    ModelState.AddModelError("", "Feil informasjon, prøv på nytt!");
+                }
+            }
+            return View(login);
+        }
+
 
         public ActionResult Contact()
         {
--- BookRoom/BookRoom/Controllers/RoomsController.cs
+++ BookRoom/BookRoom/Controllers/RoomsController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
-using System.Dynamic;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -11,121 +10,107 @@ using BookRoom.Models;
 
 namespace BookRoom.Controllers
 {
-    public class BookingsController : Controller
+    public class RoomsController : Controller
     {
         private oblig4Entities9 db = new oblig4Entities9();
 
-        // GET: Bookings
+        // GET: Rooms
         public ActionResult Index()
         {
-            var bookings = db.Bookings.Include(b => b.Rooms).Include(b => b.Customers);
-            return View(bookings.ToList());
+            return View(db.Rooms.ToList());
         }
 
-        // GET: Bookings/Details/5
+        // GET: Rooms/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Bookings bookings = db.Bookings.Find(id);
-            if (bookings == null)
+            Rooms rooms = db.Rooms.Find(id);
+            if (rooms == null)
             {
                 return HttpNotFound();
             }
-            return View(bookings);
+            return View(rooms);
         }
 
-        // GET: Bookings/Create
-
+        // GET: Rooms/Create
         public ActionResult Create()
         {
-            dynamic mymodel = new ExpandoObject();
-            ViewBag.roomID = new SelectList(db.Rooms, "RoomsID", "RoomsID");
-            ViewBag.Beds = new SelectList(db.Rooms, "Beds", "Beds");
-            ViewBag.Rooms = db.Rooms;
-            ViewBag.Size = new SelectList(db.Rooms, "size", "size");
-            ViewBag.username = new SelectList(db.Customers, "CustomersID", "CustomersID");
             return View();
         }
 
-        // POST: Bookings/Create
+        // POST: Rooms/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "BookingsID,roomID,username,bookingfrom,bookingto,checkedin,checkedout")] Bookings bookings)
+        public ActionResult Create([Bind(Include = "RoomsID,beds,size")] Rooms rooms)
         {
             if (ModelState.IsValid)
             {
-                db.Bookings.Add(bookings);
+                db.Rooms.Add(rooms);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.roomID = new SelectList(db.Rooms, "RoomsID", "RoomsID", bookings.roomID);
-            ViewBag.username = new SelectList(db.Customers, "CustomersID", "pass", bookings.username);
-            return View(bookings);
+            return View(rooms);
         }
 
-        // GET: Bookings/Edit/5
+        // GET: Rooms/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Bookings bookings = db.Bookings.Find(id);
-            if (bookings == null)
+            Rooms rooms = db.Rooms.Find(id);
+            if (rooms == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.roomID = new SelectList(db.Rooms, "RoomsID", "RoomsID", bookings.roomID);
-            ViewBag.username = new SelectList(db.Customers, "CustomersID", "pass", bookings.username);
-            return View(bookings);
+            return View(rooms);
         }
 
-        // POST: Bookings/Edit/5
+        // POST: Rooms/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "BookingsID,roomID,username,bookingfrom,bookingto,checkedin,checkedout")] Bookings bookings)
+        public ActionResult Edit([Bind(Include = "RoomsID,beds,size")] Rooms rooms)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(bookings).State = EntityState.Modified;
+                db.Entry(rooms).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.roomID = new SelectList(db.Rooms, "RoomsID", "RoomsID", bookings.roomID);
-            ViewBag.username = new SelectList(db.Customers, "CustomersID", "pass", bookings.username);
-            return View(bookings);
+            return View(rooms);
         }
 
-        // GET: Bookings/Delete/5
+        // GET: Rooms/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Bookings bookings = db.Bookings.Find(id);
-            if (bookings == null)
+            Rooms rooms = db.Rooms.Find(id);
+            if (rooms == null)
             {
                 return HttpNotFound();
             }
-            return View(bookings);
+            return View(rooms);
         }
 
-        // POST: Bookings/Delete/5
+        // POST: Rooms/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Bookings bookings = db.Bookings.Find(id);
-            db.Bookings.Remove(bookings);
+            Rooms rooms = db.Rooms.Find(id);
+            db.Rooms.Remove(rooms);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
